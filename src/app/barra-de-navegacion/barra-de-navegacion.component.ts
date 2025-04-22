182885931ea1de154553169240a5fbb4d451110c
MarioBermu <147144316+MarioBermu@users.noreply.github.com>
2025-04-22 18:05:21 +0200

    intento 1 migración a móvil

--- src/app/barra-de-navegacion/barra-de-navegacion.component.ts
+++ src/app/barra-de-navegacion/barra-de-navegacion.component.ts
@@ -1,20 +1,19 @@
-import { Component } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
-
-
 import { RouterModule } from '@angular/router';
-
 import { PageNosotrosComponent } from '../page-nosotros/page-nosotros.component';
 import { HomeComponent } from '../home/home.component';
 
+
+import { UserI } from '../models/user';
+import { Component, AfterViewInit, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { AuthService } from '../service/auth.service';
 import { Router } from '@angular/router';
-import { OnInit } from '@angular/core';
-import { UserI } from '../models/user';
 
 
 
-
+declare const bootstrap: any;
 @Component({
   selector: 'app-barra-de-navegacion',
   standalone: true,
@@ -23,9 +22,10 @@ import { UserI } from '../models/user';
   styleUrl: './barra-de-navegacion.component.css'
 })
 export class BarraDeNavegacionComponent implements OnInit {
+  private collapseInstance!: any;
   loggedInUsers: string = '';
 
-  constructor(public authService: AuthService, private router: Router) { }
+  constructor(public authService: AuthService, private router: Router, @Inject(PLATFORM_ID) private platformId: Object ) { }
 
   ngOnInit() {
     this.authService.loggedInUsers.subscribe(name => {
@@ -36,8 +36,33 @@ export class BarraDeNavegacionComponent implements OnInit {
   onLogout(): void {
     this.authService.logout();
     this.router.navigateByUrl('/home');
-    //this.router.navigateByUrl('/admin');
+    this.authService.logout();
+    this.router.navigate(['/login']);
+    this.onNavLinkClick();
   }
+
+  ngAfterViewInit(): void {
+    // si NO estamos en navegador, salimos
+    if (!isPlatformBrowser(this.platformId)) {
+      return;
+    }
+    // aquí sí hay document
+    const navEl = document.getElementById('MenuNavegacion');
+    if (navEl) {
+      this.collapseInstance = bootstrap.Collapse.getOrCreateInstance(navEl, {
+        toggle: false
+      });
+    }
+  }
+
+  onNavLinkClick(): void {
+    // tu lógica de ocultar menú; aquí sí que document existe
+    const navEl = document.getElementById('MenuNavegacion');
+    if (this.collapseInstance && navEl?.classList.contains('show')) {
+      this.collapseInstance.hide();
+    }
+  }
+
 
   get userName(): string | null {
     return this.authService.getUserName();
